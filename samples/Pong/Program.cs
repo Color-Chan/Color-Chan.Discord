df4c0ce30d4ab3fdc1c749490188790841b4caf0
Bram Esendam <Bram.esendam@gmail.com>
2021-07-21 00:48:20 +0200

    Added more extensions for ease of use

--- samples/Pong/Program.cs
+++ samples/Pong/Program.cs
@@ -1,16 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using Color_Chan.Discord.Commands.Configurations;
 using Color_Chan.Discord.Commands.Models;
-using Color_Chan.Discord.Commands.Services;
+using Color_Chan.Discord.Extensions;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Pong
 {
@@ -19,30 +13,19 @@ namespace Pong
         public static async Task Main(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
-            
-            // Create a new scope.
-            using (var scope = host.Services.CreateScope())
-            {
-                // Get the slash command service.
-                var commandService = scope.ServiceProvider.GetRequiredService<ISlashCommandService>();
 
-                // Configure the slash command service.
-                var configurations = new SlashCommandConfiguration
-                {
-                    SlashCommandsAutoSync = SlashCommandsAutoSync.AddUpdate | SlashCommandsAutoSync.Delete
-                };
-                commandService.Configure(configurations);
-                
-                // Add all commands in an assembly to the slash command service.
-                await commandService.AddInteractionCommandsAsync(Assembly.GetExecutingAssembly()).ConfigureAwait(false);
-            }
-            
+            // Configure Color-Chan.Discord.Commands
+            var config = new SlashCommandConfiguration(SlashCommandsAutoSync.AddUpdate | SlashCommandsAutoSync.Delete); // <----- 
+            await host.RegisterSlashCommandsAsync(Assembly.GetExecutingAssembly(), config).ConfigureAwait(false); // <----- 
+
             // Run the WebHost, and start accepting requests.
             await host.RunAsync().ConfigureAwait(false);
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+        private static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+        }
     }
 }
---
+++ src/Color-Chan.Discord/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace Color_Chan.Discord.Extensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+        /// <summary>
+        ///     Adds configurations needed for Color-Chan.Discord.
+        /// </summary>
+        /// <param name="app">The <see cref="IApplicationBuilder" />.</param>
+        /// <returns>
+        ///     The updated <see cref="IApplicationBuilder" />.
+        /// </returns>
+        public static IApplicationBuilder UseColorChan(this IApplicationBuilder app)
+        {
+            // Enables the ability to read the raw body data in a api controller.
+            // This is needed for the authentication.
+            app.Use((context, next) =>
+            {
+                context.Request.EnableBuffering();
+                return next();
+            });
+
+            return app;
+        }
+    }
+}
