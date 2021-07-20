d780cdf6c597e293cc1e0540980dd1fbba497dce
Bram Esendam <Bram.esendam@gmail.com>
2021-07-20 22:59:52 +0200

    Merged Color-Chan.Discord.Host into Color-Chan.Discord

---
+++ src/Color-Chan.Discord/Tokens/PublicDiscordToken.cs
@@ -0,0 +1,20 @@
+﻿namespace Color_Chan.Discord.Tokens
+{
+    public record PublicDiscordToken : IPublicDiscordToken
+    {
+        /// <summary>
+        ///     Initializes a new instance of <see cref="PublicDiscordToken" />.
+        /// </summary>
+        /// <param name="token">
+        ///     The public token of your application.
+        ///     This can be found at https://discord.com/developers/applications/{APPLICATION_ID}/information
+        /// </param>
+        public PublicDiscordToken(string token)
+        {
+            Token = token;
+        }
+
+        /// <inheritdoc />
+        public string Token { get; init; }
+    }
+}
