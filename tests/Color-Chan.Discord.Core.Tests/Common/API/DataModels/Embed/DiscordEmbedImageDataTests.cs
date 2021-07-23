9f2e9693ecbf306f245ff2b6a9517ad10b9b020d
Bram Esendam <Bram.esendam@gmail.com>
2021-07-23 17:34:35 +0200

    Added JSON unit tests for all embed models

---
+++ tests/Color-Chan.Discord.Core.Tests/Common/API/DataModels/Embed/DiscordEmbedImageDataTests.cs
@@ -0,0 +1,11 @@
+using Color_Chan.Discord.Core.Common.API.DataModels.Embed;
+using NUnit.Framework;
+
+namespace Color_Chan.Discord.Core.Tests.Common.API.DataModels.Embed
+{
+    [TestFixture]
+    public class DiscordEmbedImageDataTests : JsonTestBase<DiscordEmbedImageData>
+    {
+        
+    }
+}
---
+++ tests/Color-Chan.Discord.Core.Tests/Common/API/DataModels/Embed/DiscordEmbedProviderDataTests.cs
@@ -0,0 +1,10 @@
+using Color_Chan.Discord.Core.Common.API.DataModels.Embed;
+using NUnit.Framework;
+
+namespace Color_Chan.Discord.Core.Tests.Common.API.DataModels.Embed
+{
+    [TestFixture]
+    public class DiscordEmbedProviderDataTests : JsonTestBase<DiscordEmbedProviderData>
+    {
+    }
+}
