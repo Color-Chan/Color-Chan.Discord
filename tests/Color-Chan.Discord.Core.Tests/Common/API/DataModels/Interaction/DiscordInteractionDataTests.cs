5f8619462c0c66b72c50165d8c753751f8357ffc
Bram Esendam <Bram.esendam@gmail.com>
2021-07-23 14:46:29 +0200

    Added more data model JSON tests

---
+++ tests/Color-Chan.Discord.Core.Tests/Common/API/DataModels/Interaction/DiscordInteractionDataTests.cs
@@ -0,0 +1,10 @@
+﻿using Color_Chan.Discord.Core.Common.API.DataModels.Interaction;
+using NUnit.Framework;
+
+namespace Color_Chan.Discord.Core.Tests.Common.API.DataModels.Interaction
+{
+    [TestFixture]
+    public class DiscordInteractionDataTests :JsonTestBase<DiscordInteractionData>
+    {
+    }
+}
