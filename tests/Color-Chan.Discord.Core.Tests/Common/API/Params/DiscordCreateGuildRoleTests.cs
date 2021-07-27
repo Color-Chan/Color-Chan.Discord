d0aa6004592bdee578774dd65aa2d0446a49c09e
Bram Esendam <Bram.esendam@gmail.com>
2021-07-27 15:09:03 +0200

    Add JSON tests for DiscordCreateGuildRole

---
+++ tests/Color-Chan.Discord.Core.Tests/Common/API/Params/DiscordCreateGuildRoleTests.cs
@@ -0,0 +1,11 @@
+using Color_Chan.Discord.Core.Common.API.Params;
+using NUnit.Framework;
+
+namespace Color_Chan.Discord.Core.Tests.Common.API.Params
+{
+    [TestFixture]
+    public class DiscordCreateGuildRoleTests : JsonTestBase<DiscordCreateGuildRole>
+    {
+        
+    }
+}
