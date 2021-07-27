cbd347911dbba0b4b29f877364004d3c907ca79e
Bram Esendam <Bram.esendam@gmail.com>
2021-07-27 15:14:57 +0200

    Added JSON tests for DiscordCreateApplicationCommand

---
+++ tests/Color-Chan.Discord.Core.Tests/Common/API/Params/DiscordCreateApplicationCommandTests.cs
@@ -0,0 +1,10 @@
+using Color_Chan.Discord.Core.Common.API.Params;
+using NUnit.Framework;
+
+namespace Color_Chan.Discord.Core.Tests.Common.API.Params
+{
+    [TestFixture]
+    public class DiscordCreateApplicationCommandTests : JsonTestBase<DiscordCreateApplicationCommand>
+    {
+    }
+}
