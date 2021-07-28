8b4502fc2646735eb43a8644472b09a74d8bf6c3
Bram Esendam <Bram.esendam@gmail.com>
2021-07-28 17:23:38 +0200

    Added Color-Chan.Discord.Benchmarks project

---
+++ tests/benchmarks/Color-Chan.Discord.Benchmarks/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Color_Chan.Discord.Benchmarks
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            RunBenchmarks();
+        }
+
+        private static void RunBenchmarks()
+        {
+            //BenchmarkRunner.Run<>();
+        }
+    }
+}
