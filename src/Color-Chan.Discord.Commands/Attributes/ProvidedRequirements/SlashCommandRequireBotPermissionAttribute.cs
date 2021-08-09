4d926e2bf9c8ef450a0f6cfb39a0055028468d31
Bram <Bram.esendam@gmail.com>
2021-08-09 22:03:58 +0200

    Removed unused usings

--- src/Color-Chan.Discord.Commands/Attributes/ProvidedRequirements/SlashCommandRequireBotPermissionAttribute.cs
+++ src/Color-Chan.Discord.Commands/Attributes/ProvidedRequirements/SlashCommandRequireBotPermissionAttribute.cs
@@ -1,11 +1,9 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Color_Chan.Discord.Commands.Models.Contexts;
 using Color_Chan.Discord.Commands.Models.Results;
 using Color_Chan.Discord.Core;
-using Color_Chan.Discord.Core.Common.API.DataModels;
 using Color_Chan.Discord.Core.Common.API.DataModels.Guild;
 using Color_Chan.Discord.Core.Common.API.Rest;
 using Color_Chan.Discord.Core.Common.Models;
