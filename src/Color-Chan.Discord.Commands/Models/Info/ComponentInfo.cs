bff044b63db155996a2e0c9d294661c827022df5
Bram <Bram.esendam@gmail.com>
2021-08-14 17:03:44 +0200

    Added `IComponentInfo`

---
+++ src/Color-Chan.Discord.Commands/Models/Info/ComponentInfo.cs
@@ -0,0 +1,36 @@
+using System.Reflection;
+using Color_Chan.Discord.Core.Common.API.DataModels;
+
+namespace Color_Chan.Discord.Commands.Models.Info
+{
+    /// <inheritdoc />
+    public class ComponentInfo : IComponentInfo
+    {
+        /// <summary>
+        ///     Initializes a new instance of <see cref="SlashCommandInfo" />.
+        /// </summary>
+        /// <param name="customId">The custom id of the component.</param>
+        /// <param name="type">The type of the component.</param>
+        /// <param name="componentMethod">The method of the component.</param>
+        /// <param name="parentModule">The parent module of the component.</param>
+        public ComponentInfo(string customId, DiscordComponentType type, MethodInfo componentMethod, TypeInfo parentModule)
+        {
+            CustomId = customId;
+            Type = type;
+            ComponentMethod = componentMethod;
+            ParentModule = parentModule;
+        }
+
+        /// <inheritdoc />
+        public string CustomId { get; set; }
+        
+        /// <inheritdoc />
+        public DiscordComponentType Type { get; set; }
+
+        /// <inheritdoc />
+        public MethodInfo ComponentMethod { get; set; }
+        
+        /// <inheritdoc />
+        public TypeInfo ParentModule { get; set; }
+    }
+}
---
+++ src/Color-Chan.Discord.Commands/Models/Info/IComponentInfo.cs
@@ -0,0 +1,31 @@
+using System.Reflection;
+using Color_Chan.Discord.Core.Common.API.DataModels;
+
+namespace Color_Chan.Discord.Commands.Models.Info
+{
+    /// <summary>
+    ///     Contains all the information about a component.
+    /// </summary>
+    public interface IComponentInfo
+    {
+        /// <summary>
+        ///     The custom ID of the component.
+        /// </summary>
+        public string CustomId { get; set; }
+
+        /// <summary>
+        ///     The type of the component.
+        /// </summary>
+        public DiscordComponentType Type { get; set; }
+        
+        /// <summary>
+        ///     The <see cref="MethodInfo" /> containing the method of the component.
+        /// </summary>
+        public MethodInfo ComponentMethod { get; set; }
+
+        /// <summary>
+        ///     The component module containing the <see cref="ComponentMethod" />.
+        /// </summary>
+        public TypeInfo ParentModule { get; set; }
+    }
+}
