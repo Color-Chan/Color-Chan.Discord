819be853fdd4d874905c6843f33c9f0f49929423
Bram <Bram.esendam@gmail.com>
2021-08-14 13:33:59 +0200

    Added missing component properties

--- src/Color-Chan.Discord.Core/Common/API/DataModels/DiscordComponentData.cs
+++ src/Color-Chan.Discord.Core/Common/API/DataModels/DiscordComponentData.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Text.Json.Serialization;
+using Color_Chan.Discord.Core.Common.API.DataModels.Select;
 
 namespace Color_Chan.Discord.Core.Common.API.DataModels
 {
@@ -11,6 +12,18 @@ namespace Color_Chan.Discord.Core.Common.API.DataModels
         [JsonPropertyName("type")]
         public DiscordComponentType Type { get; init; }
 
+        /// <summary>
+        ///     A developer-defined identifier for the button, max 100 characters.
+        /// </summary>
+        [JsonPropertyName("custom_id")]
+        public string? CustomId { get; init; }
+        
+        /// <summary>
+        ///     Whether the button is disabled, default false
+        /// </summary>
+        [JsonPropertyName("disabled")]
+        public bool? Disabled { get; init; }
+        
         /// <summary>
         ///     The style the button.
         /// </summary>
@@ -30,23 +43,35 @@ namespace Color_Chan.Discord.Core.Common.API.DataModels
         public DiscordEmojiData? Emoji { get; init; }
 
         /// <summary>
-        ///     A developer-defined identifier for the button, max 100 characters.
-        /// </summary>
-        [JsonPropertyName("custom_id")]
-        public string? CustomId { get; init; }
-
-        /// <summary>
         ///     Url for link-style buttons.
         /// </summary>
         [JsonPropertyName("url")]
         public string? Url { get; init; }
 
         /// <summary>
-        ///     Whether the button is disabled, default false
+        ///     The choices in the select, max 25.
         /// </summary>
-        [JsonPropertyName("disabled")]
-        public bool? Disabled { get; init; }
-
+        [JsonPropertyName("options")]
+        public IEnumerable<DiscordSelectOptionData>? SelectOptions { get; init; }
+        
+        /// <summary>
+        ///     Custom placeholder text if nothing is selected, max 100 characters.
+        /// </summary>
+        [JsonPropertyName("placeholder")]
+        public string? Placeholder { get; init; }
+        
+        /// <summary>
+        ///     The minimum number of items that must be chosen; default 1, min 0, max 25.
+        /// </summary>
+        [JsonPropertyName("min_values")]
+        public int? MinValues { get; init; }
+        
+        /// <summary>
+        ///     The maximum number of items that can be chosen; default 1, max 25.
+        /// </summary>
+        [JsonPropertyName("max_values")]
+        public int? MaxValues { get; init; }
+        
         /// <summary>
         ///     A list of child components.
         /// </summary>
