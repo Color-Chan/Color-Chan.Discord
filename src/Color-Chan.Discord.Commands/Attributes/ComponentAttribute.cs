65e0bab89582d67f9392f96777967d08087b27f3
Bram <Bram.esendam@gmail.com>
2021-08-14 17:03:57 +0200

    Fix: Missing component type on `ComponentAttribute`

--- src/Color-Chan.Discord.Commands/Attributes/ComponentAttribute.cs
+++ src/Color-Chan.Discord.Commands/Attributes/ComponentAttribute.cs
@@ -1,4 +1,5 @@
 using System;
+using Color_Chan.Discord.Core.Common.API.DataModels;
 
 namespace Color_Chan.Discord.Commands.Attributes
 {
@@ -11,15 +12,22 @@ namespace Color_Chan.Discord.Commands.Attributes
         /// <summary>
         ///     Initializes a new instance of <see cref="SlashCommandAttribute" />.
         /// </summary>
-        /// <param name="customId">The custom id of the component that the underlying method will handle click events of.</param>
-        public ComponentAttribute(string customId)
+        /// <param name="customId">The custom id of the component that the underlying method will handle.</param>
+        /// <param name="type">The type of the component that will be handled.</param>
+        public ComponentAttribute(string customId, DiscordComponentType type)
         {
             CustomId = customId;
+            Type = type;
         }
         
         /// <summary>
         ///     The developer-defined identifier for the component, max 100 characters
         /// </summary>
         public string CustomId { get; }
+
+        /// <summary>
+        ///     The type of the component that will be handled.
+        /// </summary>
+        public DiscordComponentType Type { get; }
     }
 }
