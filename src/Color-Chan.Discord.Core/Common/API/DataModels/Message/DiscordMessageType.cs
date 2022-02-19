64cc0608692a395aec9e8980cbbb47a7b7d0be4d
Bram <bram.esendam@gmail.com>
2022-02-19 16:43:56 +0100

    Added missing message types

--- src/Color-Chan.Discord.Core/Common/API/DataModels/Message/DiscordMessageType.cs
+++ src/Color-Chan.Discord.Core/Common/API/DataModels/Message/DiscordMessageType.cs
@@ -66,10 +66,55 @@
         ///     The message for when a news channel subscription is added to a text channel.
         /// </summary>
         ChannelFollowAdd = 12,
+        
+        /// <summary>
+        ///     The message for when a server has been disqualified for discovery.
+        /// </summary>
+        GuildDiscoveryDisqualified = 14,
+        
+        /// <summary>
+        ///     The message for when a server has been re-qualified for discovery.
+        /// </summary>
+        GuildDiscoveryReQualified = 15,
+        
+        /// <summary>
+        ///     The message for when a server is getting close to being disqualified for discovery.
+        /// </summary>
+        GuildDiscoveryGracePeriodInitialWarning = 16,
+        
+        /// <summary>
+        ///     The message for when a server is almost being disqualified for discovery.
+        /// </summary>
+        GuildDiscoveryGracePeriodFinalWarning = 17,
+        
+        /// <summary>
+        ///     The message for when a thread has been created.
+        /// </summary>
+        ThreadCreated = 18,
 
         /// <summary>
         ///     The message is an inline reply.
         /// </summary>
-        Reply = 19
+        Reply = 19,
+        
+        /// <summary>
+        ///     An application command message.
+        /// </summary>
+        ChatInputCommand = 20,
+        
+        /// <summary>
+        ///     The first message for a thread.
+        /// </summary>
+        ThreadStarterMessage = 21,
+        
+        /// <summary>
+        ///     The reminder message for guild invites.
+        /// </summary>
+        GuildInviteReminder = 22,
+        
+        /// <summary>
+        ///     A message for a context menu command.
+        /// </summary>
+        ContextMenuCommand = 23
     }
 }
