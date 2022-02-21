79068c30fdd7e87f0136f988f2c05017e46235ac
Bram <bram.esendam@gmail.com>
2022-02-21 13:54:58 +0100

    Added missing sticker type data

---
+++ src/Color-Chan.Discord.Core/Common/API/DataModels/DiscordStickerFormatType.cs
@@ -0,0 +1,13 @@
+﻿namespace Color_Chan.Discord.Core.Common.API.DataModels
+{
+    /// <summary>
+    ///     Represents a discord Sticker Types API model.
+    ///     Docs: https://discord.com/developers/docs/resources/sticker#sticker-object-sticker-format-types
+    /// </summary>
+    public enum DiscordStickerType
+    {
+        Png = 1,
+        APng = 2,
+        Lottie = 3
+    }
+}
