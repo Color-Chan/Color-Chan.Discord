4da42047a727c965e925c823a378a5740e77b854
BrammyS <bram.esendam@gmail.com>
2025-08-30 13:23:07 +0200

    Added missing Guild features & System channel flags

--- src/Color-Chan.Discord.Core/Common/API/Converters/DiscordGuildFeatureConverter.cs
+++ src/Color-Chan.Discord.Core/Common/API/Converters/DiscordGuildFeatureConverter.cs
@@ -16,27 +16,36 @@ public class DiscordGuildFeatureConverter : JsonConverter<DiscordGuildFeature>
         var value = reader.GetString();
         return value switch
         {
+            "ANIMATED_BANNER" => DiscordGuildFeature.AnimatedBanner,
             "ANIMATED_ICON" => DiscordGuildFeature.AnimatedIcon,
+            "APPLICATION_COMMAND_PERMISSIONS_V2" => DiscordGuildFeature.ApplicationCommandPermissionsV2,
+            "AUTO_MODERATION" => DiscordGuildFeature.AutoModeration,
             "BANNER" => DiscordGuildFeature.Banner,
             "COMMERCE" => DiscordGuildFeature.Commerce,
             "COMMUNITY" => DiscordGuildFeature.Community,
+            "CREATOR_MONETIZABLE_PROVISIONAL" => DiscordGuildFeature.CreatorMonetizableProvisional,
+            "CREATOR_STORE_PAGE" => DiscordGuildFeature.CreatorStorePage,
+            "DEVELOPER_SUPPORT_SERVER" => DiscordGuildFeature.DeveloperSupportServer,
             "DISCOVERABLE" => DiscordGuildFeature.Discoverable,
             "FEATURABLE" => DiscordGuildFeature.Featurable,
+            "INVITES_DISABLED" => DiscordGuildFeature.InvitesDisabled,
             "INVITE_SPLASH" => DiscordGuildFeature.InviteSplash,
             "MEMBER_VERIFICATION_GATE_ENABLED" => DiscordGuildFeature.MemberVerificationGateEnabled,
+            "MORE_SOUNDBOARD" => DiscordGuildFeature.MoreSoundboard,
+            "MORE_STICKERS" => DiscordGuildFeature.MoreStickers,
             "NEWS" => DiscordGuildFeature.News,
             "PARTNERED" => DiscordGuildFeature.Partnered,
             "PREVIEW_ENABLED" => DiscordGuildFeature.PreviewEnabled,
+            "RAID_ALERTS_DISABLED" => DiscordGuildFeature.RaidAlertsDisabled,
+            "ROLE_ICONS" => DiscordGuildFeature.RoleIcons,
+            "ROLE_SUBSCRIPTIONS_AVAILABLE_FOR_PURCHASE" => DiscordGuildFeature.RoleSubscriptionsAvailableForPurchase,
+            "ROLE_SUBSCRIPTIONS_ENABLED" => DiscordGuildFeature.RoleSubscriptionsEnabled,
+            "SOUNDBOARD" => DiscordGuildFeature.Soundboard,
+            "TICKETED_EVENTS_ENABLED" => DiscordGuildFeature.TicketedEventsEnabled,
             "VANITY_URL" => DiscordGuildFeature.VanityUrl,
             "VERIFIED" => DiscordGuildFeature.Verified,
             "VIP_REGIONS" => DiscordGuildFeature.VipRegions,
             "WELCOME_SCREEN_ENABLED" => DiscordGuildFeature.WelcomeScreenEnabled,
-            "TICKETED_EVENTS_ENABLED" => DiscordGuildFeature.TicketedEventsEnabled,
-            "MONETIZATION_ENABLED" => DiscordGuildFeature.MonetizationEnabled,
-            "MORE_STICKERS" => DiscordGuildFeature.MoreStickers,
-            "THREE_DAY_THREAD_ARCHIVE" => DiscordGuildFeature.ThreeDayThreadArchive,
-            "SEVEN_DAY_THREAD_ARCHIVE" => DiscordGuildFeature.SevenDayThreadArchive,
-            "PRIVATE_THREADS" => DiscordGuildFeature.PrivateThreads,
             _ => DiscordGuildFeature.Unknown
         };
     }
@@ -46,8 +55,17 @@ public class DiscordGuildFeatureConverter : JsonConverter<DiscordGuildFeature>
     {
         switch (value)
         {
+            case DiscordGuildFeature.AnimatedBanner:
+                writer.WriteStringValue("ANIMATED_BANNER");
+                break;
             case DiscordGuildFeature.AnimatedIcon:
                 writer.WriteStringValue("ANIMATED_ICON");
+                break;
+            case DiscordGuildFeature.ApplicationCommandPermissionsV2:
+                writer.WriteStringValue("APPLICATION_COMMAND_PERMISSIONS_V2");
+                break;
+            case DiscordGuildFeature.AutoModeration:
+                writer.WriteStringValue("AUTO_MODERATION");
                 break;
             case DiscordGuildFeature.Banner:
                 writer.WriteStringValue("BANNER");
@@ -58,17 +76,35 @@ public class DiscordGuildFeatureConverter : JsonConverter<DiscordGuildFeature>
             case DiscordGuildFeature.Community:
                 writer.WriteStringValue("COMMUNITY");
                 break;
+            case DiscordGuildFeature.CreatorMonetizableProvisional:
+                writer.WriteStringValue("CREATOR_MONETIZABLE_PROVISIONAL");
+                break;
+            case DiscordGuildFeature.CreatorStorePage:
+                writer.WriteStringValue("CREATOR_STORE_PAGE");
+                break;
+            case DiscordGuildFeature.DeveloperSupportServer:
+                writer.WriteStringValue("DEVELOPER_SUPPORT_SERVER");
+                break;
             case DiscordGuildFeature.Discoverable:
                 writer.WriteStringValue("DISCOVERABLE");
                 break;
             case DiscordGuildFeature.Featurable:
                 writer.WriteStringValue("FEATURABLE");
                 break;
+            case DiscordGuildFeature.InvitesDisabled:
+                writer.WriteStringValue("INVITES_DISABLED");
+                break;
             case DiscordGuildFeature.InviteSplash:
                 writer.WriteStringValue("INVITE_SPLASH");
                 break;
             case DiscordGuildFeature.MemberVerificationGateEnabled:
                 writer.WriteStringValue("MEMBER_VERIFICATION_GATE_ENABLED");
+                break;
+            case DiscordGuildFeature.MoreSoundboard:
+                writer.WriteStringValue("MORE_SOUNDBOARD");
+                break;
+            case DiscordGuildFeature.MoreStickers:
+                writer.WriteStringValue("MORE_STICKERS");
                 break;
             case DiscordGuildFeature.News:
                 writer.WriteStringValue("NEWS");
@@ -78,6 +114,24 @@ public class DiscordGuildFeatureConverter : JsonConverter<DiscordGuildFeature>
                 break;
             case DiscordGuildFeature.PreviewEnabled:
                 writer.WriteStringValue("PREVIEW_ENABLED");
+                break;
+            case DiscordGuildFeature.RaidAlertsDisabled:
+                writer.WriteStringValue("RAID_ALERTS_DISABLED");
+                break;
+            case DiscordGuildFeature.RoleIcons:
+                writer.WriteStringValue("ROLE_ICONS");
+                break;
+            case DiscordGuildFeature.RoleSubscriptionsAvailableForPurchase:
+                writer.WriteStringValue("ROLE_SUBSCRIPTIONS_AVAILABLE_FOR_PURCHASE");
+                break;
+            case DiscordGuildFeature.RoleSubscriptionsEnabled:
+                writer.WriteStringValue("ROLE_SUBSCRIPTIONS_ENABLED");
+                break;
+            case DiscordGuildFeature.Soundboard:
+                writer.WriteStringValue("SOUNDBOARD");
+                break;
+            case DiscordGuildFeature.TicketedEventsEnabled:
+                writer.WriteStringValue("TICKETED_EVENTS_ENABLED");
                 break;
             case DiscordGuildFeature.VanityUrl:
                 writer.WriteStringValue("VANITY_URL");
@@ -90,24 +144,6 @@ public class DiscordGuildFeatureConverter : JsonConverter<DiscordGuildFeature>
                 break;
             case DiscordGuildFeature.WelcomeScreenEnabled:
                 writer.WriteStringValue("WELCOME_SCREEN_ENABLED");
-                break;
-            case DiscordGuildFeature.TicketedEventsEnabled:
-                writer.WriteStringValue("TICKETED_EVENTS_ENABLED");
-                break;
-            case DiscordGuildFeature.MonetizationEnabled:
-                writer.WriteStringValue("MONETIZATION_ENABLED");
-                break;
-            case DiscordGuildFeature.MoreStickers:
-                writer.WriteStringValue("MORE_STICKERS");
-                break;
-            case DiscordGuildFeature.ThreeDayThreadArchive:
-                writer.WriteStringValue("THREE_DAY_THREAD_ARCHIVE");
-                break;
-            case DiscordGuildFeature.SevenDayThreadArchive:
-                writer.WriteStringValue("SEVEN_DAY_THREAD_ARCHIVE");
-                break;
-            case DiscordGuildFeature.PrivateThreads:
-                writer.WriteStringValue("PRIVATE_THREADS");
                 break;
             case DiscordGuildFeature.Unknown:
                 break;
--- src/Color-Chan.Discord.Core/Common/API/DataModels/Guild/DiscordSystemChannelFlags.cs
+++ src/Color-Chan.Discord.Core/Common/API/DataModels/Guild/DiscordSystemChannelFlags.cs
@@ -27,5 +27,20 @@ public enum DiscordSystemChannelFlags
     /// <summary>
     ///     Suppress server setup tips.
     /// </summary>
-    SuppressGuildReminderNotifications = 1 << 2
+    SuppressGuildReminderNotifications = 1 << 2,
+    
+    /// <summary>
+    ///     Hide member joins sticker reply buttons.
+    /// </summary>
+    SuppressJoinNotificationReplies = 1 << 3,
+    
+    /// <summary>
+    ///     Suppress role subscription purchase and renewal notifications
+    /// </summary>
+    SuppressRoleSubscriptionPurchaseNotifications = 1 << 4,
+    
+    /// <summary>
+    ///     Hide role subscription sticker reply buttons
+    /// </summary>
+    SuppressRoleSubscriptionPurchaseNotificationReplies = 1 << 5
 }
