f1a68ac6aad5017f1236794c5064288ad7817a54
BrammyS <bram.esendam@gmail.com>
2026-05-03 17:00:32 +0200

    feat: more unit tests to validate multi assembly commands registrations

---
+++ tests/mockCommandAssemblies/Color-Chan.Discord.Commands.Tests.Valid2/ValidMockCommandModule0.cs
@@ -0,0 +1,15 @@
+﻿using Color_Chan.Discord.Commands.Attributes;
+using Color_Chan.Discord.Commands.Modules;
+using Color_Chan.Discord.Core.Common.Models.Interaction;
+using Color_Chan.Discord.Core.Results;
+
+namespace Color_Chan.Discord.Commands.Tests.Valid2;
+
+public class ValidMockCommandModule0 : SlashCommandModule
+{
+    [SlashCommand("AGoodCommand", "a unit test command.")]
+    public Task<Result<IDiscordInteractionResponse>> AGoodCommand()
+    {
+        throw new Exception();
+    }
+}
